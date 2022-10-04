f5b631db3d0283abd28309d1c4d2f9a1a4dbb714
Viet Anh Le <mc.vietanh@gmail.com>
2022-10-04 10:32:39 -0400

    basic setup

---
+++ components/common/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Head from 'next/head';
+function Header({ title, children, description }) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta charSet='utf-8' />
+
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <meta name='description' content='I am a software engineer in Canada' />
+      <meta property='og:locale' content='en_US' />
+      <meta property='og:type' content='website' />
+      <meta
+        property='og:title'
+        content='Felix Le - a Web developer - ReactJS'
+      />
+      <meta
+        property='og:description'
+        content=' I am a web developer who provide consultant and mobile and web development'
+      />
+      <meta
+        name='google-site-verification'
+        content='XMsTDnI6zOJIY2bfKtGGZe9eJG3WujClHrzBUrtwBJA'
+      />
+      <meta property='og:url' content='https://www.linkedin.com/in/felix-le/' />
+      <meta property='og:site_name' content='Felix Le' />
+      <meta property='og:image' content='/assets/images/about/url_img.png' />
+      <meta property='og:image:width' content='500' />
+      <meta property='og:image:height' content='500' />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NJH9SVV');`,
+        }}
+      ></script>
+    </Head>
+  );
+}
+
+export default Header;
---
+++ components/common/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Layout({ children }) {
+  return (
+    <>
+      <h1>Layout</h1>
+      {children}
+    </>
+  );
+}
+
+export default Layout;
